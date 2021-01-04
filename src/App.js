4eace865d1b0e42401d7231fe48b23a19f520ee0
Vipul <parnerkarvipul@gmail.com>
2021-01-04 19:47:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import { Component } from 'react';
+import ReactTable from 'react-table-6'
+ import "react-table-6/react-table.css";
+
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state={
+      posts:[],
+    }
+    
+  }
+
+  componentDidMount(){
+    fetch("https://jsonplaceholder.typicode.com/posts",{
+      method:"GET",  
+    }).then(rs => rs.json()).then(posts =>{
+      this.setState({posts:posts})
+     
+    })
+  }
+  onClick = (e)=>{
+    console.log(e.target.getAttribute('data-item'));
+
+  }
+  
+
+  render(){
+    const onRowClick = (state,rowInfo,column,instance)=>{
+      return{
+        onClick: e =>{
+          alert(rowInfo.original.title);
+          
+        }
+      }
+    
+    }
+    const columns = [
+      {
+        Header:"UserID",
+        accessor:"userId"
+      },
+      {
+        Header:"ID",
+        accessor:"id"
+      },
+      {
+        Header:"Title",
+        accessor:"title"
+      },
+      {
+        Header:"Content",
+        accessor:"body"
+      },
+      
+    ]
+    
+    return (
+       <div className="App">
+         {/* <table>
+           <thead>
+             <tr>
+               <th>ID</th>
+               <th>Title</th>
+
+              
+
+             </tr>
+           </thead>
+       <tbody>{
+            
+           this.state.posts.map((data,index )=>(
+              <tr onClick={this.onClick} data-item={data.id}style={{backgroundColor:"red"} }>
+              <td >{data.id}</td>
+               <td>{data.title}</td>
+             </tr>
+            ))
+            
+          }
+          
+
+          </tbody> 
+
+        </table> */}
+        
+
+        <ReactTable columns={columns} data={this.state.posts} getTdProps={onRowClick}>
+          
+          </ReactTable>        
+       
+      </div>
+    );
+
+  }
+ 
 }
 
 export default App;
